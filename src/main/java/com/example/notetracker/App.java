96e6f1c90a5790e5dfa2bf59328b02a04a1cd9dc
nergis <126512044+daffodil544@users.noreply.github.com>
2026-06-26 14:53:50 +0100

    Revert "note entity"
    This reverts commit e7c3b77dcfbc35cb4d7461589f2237d647f48189.

--- src/main/java/com/example/notetracker/App.java
+++ src/main/java/com/example/notetracker/App.java
@@ -3,10 +3,8 @@ package com.example.notetracker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @Slf4j
 public class App {
 
--- src/main/java/com/example/notetracker/user/repository/UserEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.notetracker.user.repository;
-
-import com.example.notetracker.user.domain.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserEntityRepository extends JpaRepository<UserEntity, String> {
-}
